ec11b79c2c19f6fadb87c137e6949cb053ac38ea
freddie784 <83510059+freddie784@users.noreply.github.com>
2022-01-12 21:50:49 -0500

    created a header and input for html page. Created an event listener on java script to recieve user input on button click

--- static/java/calculator.js
+++ static/java/calculator.js
@@ -1,5 +1,17 @@
-function getBushels() {
-    var bushels = document.getElementById("myNumber").value;
-    console.log(bushels *5 );
-  }
+var number_of_bushels = document.querySelector('totalBushels')
 
+function submit(number_of_bushels) {
+  console.log(number_of_bushels)
+};
+
+d3.select("button").on("click", () => {
+  let number_of_bushels = document.getElementsByClassName('totalBushels')[0].value;
+  submit(number_of_bushels)
+});
+
+
+
+
+
+per_bushel_drying_cost = .01;
+heat_cost = .73;
