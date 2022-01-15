f71c2e84804ff04fbc5d9fe1dfd80c29832fe770
freddie784 <83510059+freddie784@users.noreply.github.com>
2022-01-14 21:49:23 -0500

    addded scroll down options and began working on functions for calculating specific moistures

--- static/java/calculator.js
+++ static/java/calculator.js
@@ -1,13 +1,24 @@
 function submit() {
-  number_of_bushels = document.getElementsByClassName('totalBushels')[0].value;
-  bushels = number_of_bushels;
+  bushels = document.getElementsByClassName('totalBushels')[0].value;
   fuelType = document.getElementsByClassName('fuelType')[0].value;
   grainType = document.getElementsByClassName('grainType')[0].value;
-console.log(number_of_bushels,fuelType,grainType)
+cornRange(bushels, fuelType)
 
 };
 
-function calculate(bushels) {
+// type = "range" min='16' max='35' step='.5' list = 'tickmarks'
+
+function cornRange(bushels, fuel) {
+  document.getElementsByClassName('grainMoisture').type = 'range';
+  document.getElementsByClassName('grainMoisture').min = '16';
+  document.getElementsByClassName('grainMoisture').max = '35';
+  document.getElementsByClassName('grainMoisture').step = '.5';
+  document.getElementsByClassName('grainMoisture').list = 'tickmarks';
+  moistureDriedTo = document.getElementsByClassName('grainMoisture')[0].value;
+  console.log(bushels)
+}
+
+function calculateSoybeans(bushels) {
 
 }
 
